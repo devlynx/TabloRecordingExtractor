322b206a42273557f13336d8d4acb4c44ca4a3c1
rksmith <ryan.smith@commercebank.com>
2015-02-22 13:47:37 -0600

    Initial Project commit

---
+++ TabloRecordingExtractor/MainWindow.xaml.cs
@@ -0,0 +1,311 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace TabloRecordingExtractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string tabloIPAddress = "192.168.1.184";
+        bool showMovies = false;
+        bool showTV = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FindRecordings_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> foundRecordings = GetRecordingList(tabloIPAddress);
+            List<Recording> recordings = new List<Recording>();
+
+            foreach (var foundRecording in foundRecordings)
+            {
+                RecordingMetadata metadata = GetRecordingMetadata(tabloIPAddress, foundRecording);
+                if(metadata != null)
+                {
+                    /*
+                    ListBoxItem newItem = new ListBoxItem();
+                    newItem.Tag = foundRecording;
+                    if(metadata.recEpisode != null)
+                    {
+                        newItem.Content = String.Format("{0} - S{1}E{2} - {3} ({4}).mp4", metadata.recSeries.jsonForClient.title, metadata.recEpisode.jsonForClient.seasonNumber.ToString("00"), metadata.recEpisode.jsonForClient.episodeNumber.ToString("00"), metadata.recEpisode.jsonForClient.title, metadata.recEpisode.jsonForClient.airDate);
+                        lbRecordingsFound.Items.Add(newItem);
+                    }
+                    else if (metadata.recMovie != null)
+                    {
+                        newItem.Content = String.Format("{0} ({1}).mp4", metadata.recMovie.jsonForClient.title, metadata.recMovie.jsonForClient.releaseYear);
+                        lbRecordingsFound.Items.Add(newItem);
+                    }
+                    */
+
+                    Recording recording = new Recording();
+                    recording.Id = foundRecording;
+                    if (metadata.recEpisode != null)
+                    {
+                        recording.Description = String.Format("{0} - S{1}E{2} - {3}", metadata.recSeries.jsonForClient.title, metadata.recEpisode.jsonForClient.seasonNumber.ToString("00"), metadata.recEpisode.jsonForClient.episodeNumber.ToString("00"), metadata.recEpisode.jsonForClient.title);
+                        recording.RecordedOnDate = DateTime.Parse(metadata.recEpisode.jsonForClient.airDate);
+                    }
+                    else if (metadata.recMovie != null)
+                    {
+                        recording.Description = String.Format("{0} ({1})", metadata.recMovie.jsonForClient.title, metadata.recMovie.jsonForClient.releaseYear);
+                        recording.RecordedOnDate = DateTime.Parse(metadata.recMovieAiring.jsonForClient.airDate);
+                    }
+                    recordings.Add(recording);
+                }
+            }
+            lvRecordingsFound.ItemsSource = recordings;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvRecordingsFound.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Description", ListSortDirection.Ascending));
+        }
+
+        private void Extract_Click(object sender, RoutedEventArgs e)
+        {
+            //// Instantiate the dialog box
+            //StatusDialogWindow dlg = new StatusDialogWindow();
+
+            //// Configure the dialog box
+            //dlg.Owner = this;
+            ////dlg.
+
+            //// Open the dialog box modally 
+            //dlg.ShowDialog();
+
+            foreach (ListBoxItem itemToExtract in lbRecordingsToExtract.Items)
+            {
+                int recordingID = (int)itemToExtract.Tag;
+                RecordingMetadata metadata = GetRecordingMetadata(tabloIPAddress, recordingID);
+                string OutputFile = String.Format("{0}\\{1} - S{2}E{3} - {4}.mp4", OutputDirectory.Text, CleanFileName(metadata.recSeries.jsonForClient.title), metadata.recEpisode.jsonForClient.seasonNumber.ToString("00"), metadata.recEpisode.jsonForClient.episodeNumber.ToString("00"), CleanFileName(metadata.recEpisode.jsonForClient.title));
+                GetRecordingVideo(tabloIPAddress, recordingID, OutputFile);
+            }
+        }
+
+        private void OutputDirectory_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            OutputDirectory.Text = dialog.SelectedPath;
+        }
+
+        private List<int> GetRecordingList(string IPAddress)
+        {
+            List<int> recordingIDs = new List<int>();
+            string webPageText;
+            using (WebClient client = new WebClient())
+            {
+                webPageText = client.DownloadString("http://" + IPAddress + ":18080/pvr");
+            }
+
+            foreach (var line in webPageText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None))
+            {
+                if (line.Contains("<tr><td class=\"n\"><a href=\""))
+                {
+                    string sRecordingID = line.Split(new string[] { "<a href=\"", "/\">" }, StringSplitOptions.None)[1];
+                    int iRecordingID;
+                    if (Int32.TryParse(sRecordingID, out iRecordingID))
+                    {
+                        recordingIDs.Add(iRecordingID);
+                    }
+                }
+            }
+            return recordingIDs;
+        }
+
+        private RecordingMetadata GetRecordingMetadata(string IPAddress, int RecordingID)
+        {
+            string metadata;
+            using (WebClient client = new WebClient())
+            {
+                try 
+                {
+                    metadata = client.DownloadString("http://" + IPAddress + ":18080/pvr/" + RecordingID.ToString() + "/meta.txt");
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return JsonConvert.DeserializeObject<RecordingMetadata>(metadata);
+        }
+
+        private void GetRecordingVideo(string IPAddress, int RecordingID, string OutputFile)
+        {
+            if (!Directory.Exists(Path.GetTempPath() + "\\TempTabloExtract"))
+            {
+                Directory.CreateDirectory(Path.GetTempPath() + "\\TempTabloExtract");
+            }
+
+            string webPageText;
+            using (WebClient client = new WebClient())
+            {
+                webPageText = client.DownloadString("http://" + IPAddress + ":18080/pvr/" + RecordingID.ToString() + "/segs/");
+            }
+
+            List<string> tsFileNames = new List<string>();
+            foreach (var line in webPageText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None))
+            {
+                if (line.Contains("video/MP2T"))
+                {
+                    string tsFileName = line.Split(new string[] { "<a href=\"", ".ts" }, StringSplitOptions.None)[1] + ".ts";
+                    tsFileNames.Add(tsFileName);
+                }
+            }
+
+            List<string> outputFileNames = new List<string>();
+            foreach (var tsFileName in tsFileNames)
+            {
+                using (WebClient Client = new WebClient())
+                {
+                    string downloadURL = String.Format("http://{0}:18080/pvr/{1}/segs/{2}", IPAddress, RecordingID.ToString(), tsFileName);
+                    string outputFileName = String.Format("{0}\\TempTabloExtract\\{1}-{2}", Path.GetTempPath(), RecordingID.ToString(), tsFileName);
+
+                    Client.DownloadFile(downloadURL, outputFileName);
+                    outputFileNames.Add(outputFileName);
+                }
+            }
+
+            ProcessVideosInFFMPEG(outputFileNames, OutputFile);
+
+            foreach (var outputFileName in outputFileNames)
+            {
+                if (File.Exists(outputFileName))
+                {
+                    File.Delete(outputFileName);
+                }
+            }
+
+            ProcessVideoInHandbrake(OutputFile, OutputFile.Replace(".mp4", ".mkv"));
+
+            if (File.Exists(OutputFile))
+            {
+                File.Delete(OutputFile);
+            }
+
+        }
+
+        private void ProcessVideosInFFMPEG(List<string> tsFileNames, string OutputFile)
+        {
+            string fileNamesConcatString = String.Join("|", tsFileNames.Select(x => x.ToString()).ToArray());
+            
+            using(Process proc = new Process())
+            { 
+                proc.StartInfo.FileName = "C:\\Temp\\ffmpeg\\bin\\ffmpeg.exe";
+                proc.StartInfo.Arguments = String.Format("-y -i \"concat:{0}\" -bsf:a aac_adtstoasc -c copy \"{1}\"", fileNamesConcatString, OutputFile);
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.UseShellExecute = false;
+                if (!proc.Start())
+                {
+                    MessageBox.Show("Error starting ffmpeg");
+                    return;
+                }
+                StreamReader reader = proc.StandardError;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //lbRecordingIDs.Items.Add(line);
+                    //Console.WriteLine(line);
+                }
+                proc.Close();
+            }
+        }
+
+        private void ProcessVideoInHandbrake(string InputMP4File, string OutputMKVFile)
+        {
+            using(Process proc = new Process())
+            { 
+                proc.StartInfo.FileName = "C:\\Program Files\\Handbrake\\HandBrakeCLI.exe";
+                proc.StartInfo.Arguments = String.Format("-i \"{0}\" -f -a 1 -E copy -f mkv -O -e x264 -q 22.0 --loose-anamorphic --modulus 2 -m --x264-preset medium --h264-profile high --h264-level 4.1 --decomb --denoise=weak -v -o \"{1}\"", InputMP4File, OutputMKVFile);
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.UseShellExecute = false;
+                if (!proc.Start())
+                {
+                    MessageBox.Show("Error starting Handbrake");
+                    return;
+                }
+                StreamReader reader = proc.StandardError;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //lbRecordingIDs.Items.Add(line);
+                    //Console.WriteLine(line);
+                }
+                proc.Close();
+            }
+        }
+
+        private static string CleanFileName(string fileName)
+        {
+            return System.IO.Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(), string.Empty));
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            //ListBoxItem itemToMove = (ListBoxItem)lbRecordingsFound.SelectedItem;
+            //ListBoxItem newItem = new ListBoxItem();
+            //newItem.Tag = itemToMove.Tag;
+            //newItem.Content = itemToMove.Content;
+            //lbRecordingsToExtract.Items.Add(newItem);
+            //lbRecordingsFound.Items.RemoveAt(lbRecordingsFound.SelectedIndex);
+            List<Recording> recordings = new List<Recording>();
+            
+
+            foreach (var selectedItem in lvRecordingsFound.SelectedItems)
+            {
+                Recording recording = selectedItem as Recording;
+
+            }
+            /*
+                    recording.Id = foundRecording;
+                    if (metadata.recEpisode != null)
+                    {
+                        recording.Description = String.Format("{0} - S{1}E{2} - {3}", metadata.recSeries.jsonForClient.title, metadata.recEpisode.jsonForClient.seasonNumber.ToString("00"), metadata.recEpisode.jsonForClient.episodeNumber.ToString("00"), metadata.recEpisode.jsonForClient.title);
+                        recording.RecordedOnDate = DateTime.Parse(metadata.recEpisode.jsonForClient.airDate);
+                    }
+                    else if (metadata.recMovie != null)
+                    {
+                        recording.Description = String.Format("{0} ({1})", metadata.recMovie.jsonForClient.title, metadata.recMovie.jsonForClient.releaseYear);
+                        recording.RecordedOnDate = DateTime.Parse(metadata.recMovieAiring.jsonForClient.airDate);
+                    }
+                    recordings.Add(recording);
+             * */
+            lvRecordingsFound.ItemsSource = recordings;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvRecordingsFound.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Description", ListSortDirection.Ascending));
+        }
+
+        private void RemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            //ListBoxItem itemToMove = (ListBoxItem)lbRecordingsToExtract.SelectedItem;
+            //ListBoxItem newItem = new ListBoxItem();
+            //newItem.Tag = itemToMove.Tag;
+            //newItem.Content = itemToMove.Content;
+            //lbRecordingsFound.Items.Add(newItem);
+            //lbRecordingsToExtract.Items.RemoveAt(lbRecordingsToExtract.SelectedIndex);
+
+        }
+    }
+
+    public class Recording
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime RecordedOnDate { get; set; }
+    }
+}
---
+++ TabloRecordingExtractor/RecordingMetadata.cs
@@ -0,0 +1,340 @@
+﻿using System.Collections.Generic;
+
+namespace TabloRecordingExtractor
+{
+    public class Relationships
+    {
+        public int recSeason { get; set; }
+        public int recSeries { get; set; }
+        public int recMovie { get; set; }
+        public int recChannel { get; set; }
+    }
+
+    public class Video
+    {
+        public string state { get; set; }
+        public long size { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public double duration { get; set; }
+        public double scheduleOffsetStart { get; set; }
+        public double scheduleOffsetEnd { get; set; }
+    }
+
+    public class User
+    {
+        public string type { get; set; }
+        public bool watched { get; set; }
+        public bool @protected { get; set; }
+        public double position { get; set; }
+    }
+
+    public class JsonForClient
+    {
+        public string airDate { get; set; }
+        public string description { get; set; }
+        public int episodeNumber { get; set; }
+        public string originalAirDate { get; set; }
+        public double scheduleDuration { get; set; }
+        public int seasonNumber { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public Relationships relationships { get; set; }
+        public Video video { get; set; }
+        public User user { get; set; }
+        public int objectID { get; set; }
+        public List<string> qualifiers { get; set; }
+    }
+
+    public class Image
+    {
+        public string type { get; set; }
+        public int imageID { get; set; }
+        public string imageType { get; set; }
+        public string imageStyle { get; set; }
+    }
+
+    public class ImageJson
+    {
+        public List<Image> images { get; set; }
+    }
+
+    public class QualityRating
+    {
+        public string ratingsBody { get; set; }
+        public string value { get; set; }
+    }
+    
+    public class Program
+    {
+        public string descriptionLang { get; set; }
+        public string entityType { get; set; }
+        public int episodeNum { get; set; }
+        public string episodeTitle { get; set; }
+        public List<string> genres { get; set; }
+        public string longDescription { get; set; }
+        public string origAirDate { get; set; }
+        public string rootId { get; set; }
+        public int seasonNum { get; set; }
+        public string seriesId { get; set; }
+        public string shortDescription { get; set; }
+        public string subType { get; set; }
+        public string title { get; set; }
+        public string titleLang { get; set; }
+        public string tmsId { get; set; }
+        public List<string> topCast { get; set; }
+        public QualityRating qualityRating { get; set; }
+        public int releaseYear { get; set; }
+    }
+
+    public class Rating
+    {
+        public string body { get; set; }
+        public string code { get; set; }
+    }
+
+    public class Station
+    {
+        public string callSign { get; set; }
+        public string stationId { get; set; }
+    }
+
+    public class JsonFromTribune
+    {
+        public List<string> channels { get; set; }
+        public int duration { get; set; }
+        public string endTime { get; set; }
+        public Program program { get; set; }
+        public List<string> qualifiers { get; set; }
+        public List<Rating> ratings { get; set; }
+        public string startTime { get; set; }
+        public Station station { get; set; }
+        public string stationId { get; set; }
+    }
+
+    public class RecEpisode
+    {
+        public JsonForClient jsonForClient { get; set; }
+        public ImageJson imageJson { get; set; }
+        public JsonFromTribune jsonFromTribune { get; set; }
+    }
+
+    public class RecMovieAiring
+    {
+        public JsonForClient jsonForClient { get; set; }
+        public ImageJson imageJson { get; set; }
+        public JsonFromTribune jsonFromTribune { get; set; }
+    }
+    
+    public class Award
+    {
+        public string category { get; set; }
+        public string name { get; set; }
+        public string nominee { get; set; }
+        public bool won { get; set; }
+        public int year { get; set; }
+    }
+
+    public class Relationships2
+    {
+        public List<int> genres { get; set; }
+    }
+
+    public class JsonForClient2
+    {
+        public string description { get; set; }
+        public double duration { get; set; }
+        public string originalAirDate { get; set; }
+        public string title { get; set; }
+        public List<string> cast { get; set; }
+        public List<Award> awards { get; set; }
+        public Relationships2 relationships { get; set; }
+        public int objectID { get; set; }
+        public string type { get; set; }
+        public string mpaaRating { get; set; }
+        public string plot { get; set; }
+        public int releaseYear { get; set; }
+        public double runtime { get; set; }
+        public List<string> directors { get; set; }
+        public double qualityRating { get; set; }
+    }
+
+    public class Image2
+    {
+        public string type { get; set; }
+        public int imageID { get; set; }
+        public string imageType { get; set; }
+        public string imageStyle { get; set; }
+    }
+
+    public class ImageJson2
+    {
+        public List<Image2> images { get; set; }
+    }
+
+    public class Rating2
+    {
+        public string body { get; set; }
+        public string code { get; set; }
+    }
+
+    public class Recommendation
+    {
+        public string tmsId { get; set; }
+        public string rootId { get; set; }
+        public string title { get; set; }
+    }
+
+    public class PreferredImage
+    {
+        public string uri { get; set; }
+        public string height { get; set; }
+        public string width { get; set; }
+        public string primary { get; set; }
+        public string category { get; set; }
+        public string tier { get; set; }
+        public Caption caption { get; set; }
+    }
+
+    public class Award2
+    {
+        public string awardId { get; set; }
+        public string awardName { get; set; }
+        public string category { get; set; }
+        public string awardCatId { get; set; }
+        public string year { get; set; }
+        public string recipient { get; set; }
+        public string personId { get; set; }
+        public string won { get; set; }
+    }
+
+    public class QualityRating2
+    {
+        public string ratingsBody { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Cast
+    {
+        public string personId { get; set; }
+        public string nameId { get; set; }
+        public string name { get; set; }
+        public string role { get; set; }
+        public string characterName { get; set; }
+        public string billingOrder { get; set; }
+    }
+
+    public class Crew
+    {
+        public string personId { get; set; }
+        public string nameId { get; set; }
+        public string name { get; set; }
+        public string role { get; set; }
+        public string billingOrder { get; set; }
+    }
+
+    public class Keywords
+    {
+        public List<string> Mood { get; set; }
+        public List<string> Theme { get; set; }
+        public List<string> Subject { get; set; }
+        public List<string> Setting { get; set; }
+        public List<string> Character { get; set; }
+    }
+
+    public class JsonFromTribune2
+    {
+        public string tmsId { get; set; }
+        public string rootId { get; set; }
+        public string seriesId { get; set; }
+        public string title { get; set; }
+        public string titleLang { get; set; }
+        public string shortDescription { get; set; }
+        public string longDescription { get; set; }
+        public string descriptionLang { get; set; }
+        public string subType { get; set; }
+        public List<Rating2> ratings { get; set; }
+        public List<string> genres { get; set; }
+        public List<Recommendation> recommendations { get; set; }
+        public PreferredImage preferredImage { get; set; }
+        public string origAirDate { get; set; }
+        public List<Award2> awards { get; set; }
+        public List<Cast> cast { get; set; }
+        public List<Crew> crew { get; set; }
+        public Keywords keywords { get; set; }
+        public string totalSeasons { get; set; }
+        public int totalEpisodes { get; set; }
+        public string entityType { get; set; }
+        public int releaseYear { get; set; }
+        public QualityRating2 qualityRating { get; set; }
+        public List<string> advisories { get; set; }
+        public List<string> directors { get; set; }
+        public string runTime { get; set; }
+    }
+
+    public class Caption
+    {
+        public string content { get; set; }
+        public string lang { get; set; }
+    }
+
+    public class Caption2
+    {
+        public string content { get; set; }
+        public string lang { get; set; }
+    }
+
+    public class ImageJsonFromTribune
+    {
+        public string uri { get; set; }
+        public string height { get; set; }
+        public string width { get; set; }
+        public string primary { get; set; }
+        public string category { get; set; }
+        public string tier { get; set; }
+        public Caption caption { get; set; }
+    }
+
+    public class RecSeries
+    {
+        public JsonForClient2 jsonForClient { get; set; }
+        public ImageJson2 imageJson { get; set; }
+        public JsonFromTribune2 jsonFromTribune { get; set; }
+        public List<ImageJsonFromTribune> imageJsonFromTribune { get; set; }
+    }
+
+    public class Relationships3
+    {
+        public int recSeries { get; set; }
+    }
+
+    public class JsonForClient3
+    {
+        public int seasonNumber { get; set; }
+        public Relationships3 relationships { get; set; }
+        public int objectID { get; set; }
+        public string type { get; set; }
+    }
+
+    public class RecSeason
+    {
+        public JsonForClient3 jsonForClient { get; set; }
+    }
+
+    public class RecMovie
+    {
+        public JsonForClient2 jsonForClient { get; set; }
+        public ImageJson2 imageJson { get; set; }
+        public JsonFromTribune2 jsonFromTribune { get; set; }
+        public List<ImageJsonFromTribune> imageJsonFromTribune { get; set; }
+    }
+
+    public class RecordingMetadata
+    {
+        public RecEpisode recEpisode { get; set; }
+        public RecSeries recSeries { get; set; }
+        public RecSeason recSeason { get; set; }
+        public RecMovieAiring recMovieAiring { get; set; }
+        public RecMovie recMovie { get; set; }
+    }
+}
